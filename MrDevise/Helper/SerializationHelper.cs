fc560a4fef255c8c6dcff814a88719c4d97aa1cc
Chris <stamps1105@gmail.com>
2016-11-10 00:28:24 +0800

    Add files via upload

---
+++ MrDevise/Helper/SerializationHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+using System.Xml;
+
+namespace MrDevise.Helper
+{
+    public class SerializationHelper
+    {
+        public static object XmlDeserialize(Type t, string path)
+        {
+            TextReader reader = null;
+            try
+            {
+                XmlSerializer deserializer = new XmlSerializer(t);
+                reader = new StreamReader(path);
+                return deserializer.Deserialize(reader);
+            }
+            finally
+            {
+                if (reader != null)
+                    reader.Close();
+            }
+        }
+    }
+}
